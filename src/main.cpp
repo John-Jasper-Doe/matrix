b83d95452bc7398d77d7e75ded5ac4e19c04a65d
John Doe <john.jasper.doe@gmail.com>
2019-05-20 04:55:39 +0400

    Added coment intro main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
+/**
+ ******************************************************************************
+ * @file    main.cpp
+ * @author  Maxim <aveter@bk.ru>
+ * @date    19/05/2019
+ * @brief   The main project file is "Matrix".
+ ******************************************************************************
+ */
+
+#include "sparse_matrix.h"
+
 #include <iostream>
 #include <cassert>
 
+
+/** @brief Main function / entry point. */
 int main()
 {
   return 0;
