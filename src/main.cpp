66833bc8ffb678871536e7edb0f9630454ef3899
Maxim Gusev <mgusev@rpkb.ru>
2019-05-20 15:07:34 +0300

    Added initalize matrix on main file

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
  ******************************************************************************
  */
 
-#include "sparse_matrix.h"
+#include "sparse_matrix.hpp"
 
 #include <iostream>
 #include <cassert>
@@ -16,6 +16,19 @@
 /** @brief Main function / entry point. */
 int main()
 {
+  const int num_elem = 10;
+  sparse_matrix<int> matrix;
+
+  /* Initialization of the main diagonal of the matrix */
+  for (size_t i = 0; i < num_elem; ++i) {
+    matrix[i][i] = static_cast<int>(i);
+  }
+
+  /* Initialization of the secondary diagonal of the matrix */
+  for (size_t i = 0; i < num_elem; ++i) {
+    matrix[i][(num_elem - 1) - i] = static_cast<int>((num_elem - 1) - i);
+  }
+
   return 0;
 }
 
