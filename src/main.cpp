5dcfd11d2d19850afd0d29d0f617cf6fd483772f
Maxim Gusev <mgusev@rpkb.ru>
2019-05-28 09:41:57 +0300

    Added trmplate function of the 'print_matrix'

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,26 @@
 #include <iomanip>
 
 
-/** @brief Main function / entry point. */
+/**
+ * @brief print_matrix
+ * @param matrix
+ */
+template<typename T, T DEFAULT_VALUE = -1, size_t MATRIX_SIZE = 2>
+void print_matrix(sparse_matrix<T, DEFAULT_VALUE, MATRIX_SIZE> &matrix)
+{
+  for (size_t j = 0; j < matrix.n_size(); ++j) {
+    for (size_t i = 0; i < matrix.m_size(); ++i) {
+      std::cout << std::setw(3) << matrix[i][j] << ' ';
+    }
+    std::cout << '\n';
+  }
+  std::cout << std::endl;
+}
+
+
+/**
+ * @brief Main function / entry point.
+ */
 int main()
 {
   /* Example of initialization by the list of a one-dimensional matrix. */
@@ -22,28 +41,14 @@ int main()
 
   std::cout << "Matrix m1{2,4,7,9}:" << std::endl;
   std::cout << "Size m1: " << m1.m_size() << "x" <<  m1.n_size() << std::endl;
-
-  for (size_t j = 0; j < m1.n_size(); ++j) {
-    for (size_t i = 0; i < m1.m_size(); ++i) {
-      std::cout << std::setw(3) << m1[i][j] << ' ';
-    }
-    std::cout << '\n';
-  }
-  std::cout << std::endl;
+  print_matrix(m1);
 
   /* Example of initialization by the list of a multidimensional matrix. */
   sparse_matrix<int> m2{{3,4,5,6,7}, {2,4,7,9}, {1,2,3,4,5}};
 
   std::cout << "Matrix m2{{3,4,5,6,7}, {2,4,7,9}, {1,2,3,4,5}}:" << std::endl;
   std::cout << "Size m2: " << m2.m_size() << "x" <<  m2.n_size() << std::endl;
-
-  for (size_t j = 0; j < m2.n_size(); ++j) {
-    for (size_t i = 0; i < m2.m_size(); ++i) {
-      std::cout << std::setw(3) << m2[i][j] << ' ';
-    }
-    std::cout << '\n';
-  }
-  std::cout << std::endl;
+  print_matrix(m2);
 
   /* Example of initialization by index of a multidimensional matrix. */
   sparse_matrix<int> m3;
@@ -55,8 +60,8 @@ int main()
     }
   }
 
-  std::cout << "Size m2: " << m2.m_size() << "x" <<  m2.n_size() << std::endl;
-
+  std::cout << "Size m3: " << m3.m_size() << "x" <<  m3.n_size() << std::endl;
+  print_matrix(m3);
 
 
   return 0;
