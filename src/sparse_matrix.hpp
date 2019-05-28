26c74624b6882bcfbd9fc0bcb2aa90d9ebf927d9
Maxim Gusev <mgusev@rpkb.ru>
2019-05-28 15:20:50 +0300

    Draft matrix

--- src/sparse_matrix.hpp
+++ src/sparse_matrix.hpp
@@ -14,24 +14,25 @@
 #include <map>
 #include <utility>
 #include <tuple>
+#include <iostream>
+#include <initializer_list>
 
 
 /**
- *  @brief The sparse matrix template class.
- *  @tparam value_type_t - matrix element type.
- *  @tparam default_value - default value (default -1).
- *  @tparam matrix_size - matrix size (default 2).
+ * @brief The sparse matrix template class.
+ * @tparam T - matrix element type.
+ * @tparam DEFAULT_VALUE - default value (default -1).
+ * @tparam MATRIX_SIZE - matrix size (default 2).
  */
-template<typename value_type_t, value_type_t default_value = -1,
-                                                        size_t matrix_size = 2>
+template<typename T, T DEFAULT_VALUE = -1, size_t MATRIX_SIZE = 2>
 class sparse_matrix
 {
   private:
     /**
-     *  @brief The struct for generate other index type for matrix.
-     *  @tparam index_type_t - index type.
-     *  @tparam index - index.
-     *  @tparam types_t - other input tupes.
+     * @brief The struct for generate other index type for matrix.
+     * @tparam index_type_t - index type.
+     * @tparam index - index.
+     * @tparam types_t - other input tupes.
      */
     template<typename index_type_t, unsigned index, typename... types_t>
     struct generate_index_type {
@@ -42,9 +43,9 @@ class sparse_matrix
     };
 
     /**
-     *  @brief The structure for generating an index type with an index of 0.
-     *  @tparam index_type_t - index type.
-     *  @tparam types_t - other input tupes.
+     * @brief The structure for generating an index type with an index of 0.
+     * @tparam index_type_t - index type.
+     * @tparam types_t - other input tupes.
      */
     template<typename index_type_t, typename... types_t>
     struct generate_index_type<index_type_t, 0, types_t...> {
@@ -52,30 +53,36 @@ class sparse_matrix
     };
 
     /**
-     *  @brief Class to work with matrix index.
-     *  @tparam num - index number.
-     *  @tparam types_t - index types.
+     * @brief Class to work with matrix index.
+     * @tparam num - index number.
+     * @tparam types_t - index types.
      */
     template<size_t num, typename... types_t>
     class index_matrix;
 
     /**
-     *  @brief Class to work with matrix index, if index = 0.
-     *  @tparam types_t - index types.
+     * @brief Class to work with matrix index, if index = 0.
+     * @tparam types_t - index types.
      */
     template<typename... types_t>
     class index_matrix<0, types_t...>;
 
     /**
-     *  Class that implements an iterator for working with matrix data.
+     * Class that implements an iterator for working with matrix data.
      */
     class iterator_matrix;
 
+    /**
+     * Class that implements an const iterator for working with matrix data.
+     */
+    class const_iterator_matrix;
+
 
     /* Aliases */
-    using index_t = typename generate_index_type<size_t, matrix_size>::type;
-    using contener_t = typename std::map<index_t, value_type_t>;
-    using next_index = index_matrix<matrix_size - 1, size_t>;
+    using index_t = typename generate_index_type<size_t, MATRIX_SIZE>::type;
+    using contener_t = typename std::map<index_t, T>;
+    using next_index = index_matrix<MATRIX_SIZE - 1, size_t>;
+    using init_list_t = typename std::initializer_list<T>;
 
 
   public:
@@ -84,16 +91,37 @@ class sparse_matrix
      */
     sparse_matrix() = default;
 
+    sparse_matrix(const init_list_t &list) {
+      size_t idx_m = 0;
+      for (auto &value : list) {
+        add(idx_m, 0, value);
+        ++idx_m;
+      }
+    }
+
+
+    sparse_matrix(const std::initializer_list<const init_list_t> &list) {
+      size_t idx_n = 0;
+      for (auto &sub_list : list) {
+        size_t idx_m = 0;
+        for (auto &value : sub_list) {
+          add(idx_m, idx_n, value);
+          ++idx_m;
+        }
+        ++idx_n;
+      }
+    }
+
     /**
-     *  @brief Copy constructor.
-     *  @param other [in] - the object to copy.
+     * @brief Copy constructor.
+     * @param other [in] - the object to copy.
      */
     sparse_matrix(const sparse_matrix &other)
       : data_(other.data_) {}
 
     /**
-     *  @brief Move constructor.
-     *  @param other [in] - the object to move.
+     * @brief Move constructor.
+     * @param other [in] - the object to move.
      */
     sparse_matrix(sparse_matrix &&other)
       : data_(std::move(other.data_)) {}
@@ -104,19 +132,17 @@ class sparse_matrix
     virtual ~sparse_matrix() = default;
 
     /**
-     *  @brief Copy operator.
-     *  @param other [in] - the object to copy.
+     * @brief Copy operator.
+     * @param other [in] - the object to copy.
      */
     sparse_matrix & operator=(const sparse_matrix &other) {
-      if (*this != other)
-        data_ = other.data_;
-
+      data_ = other.data_;
       return *this;
     }
 
     /**
-     *  @brief Move operator.
-     *  @param other [in] - the object to move.
+     * @brief Move operator.
+     * @param other [in] - the object to move.
      */
     sparse_matrix & operator=(sparse_matrix &&other) {
       data_ = std::move(other.data_);
@@ -124,24 +150,100 @@ class sparse_matrix
     }
 
     /**
-     *  @brief Comparison operator.
-     *  @param other [in] - the object to move.
+     * @brief
+     * @param other [in] -
+     */
+    sparse_matrix operator*(const int value) {
+      sparse_matrix tmp;
+      auto it_m = begin();
+      for (; it_m != end(); ++it_m) {
+        tmp[std::get<0>(*it_m)][std::get<1>(*it_m)] = std::get<2>(*it_m) *
+                                                      value;
+      }
+      return tmp;
+    }
+
+    /**
+     * @brief operator *
+     * @param value
+     * @return
+     */
+    sparse_matrix operator*=(const int value) {
+      auto it_m = begin();
+      for (; it_m != end(); ++it_m) {
+         std::get<2>(*it_m) *= value;
+      }
+      return *this;
+    }
+
+    /**
+     * @brief
+     * @param other [in] -
+     */
+    sparse_matrix operator+(const sparse_matrix &other) {
+      const std::tuple<size_t, size_t> m1 = size();
+      const std::tuple<size_t, size_t> m2 = other.size();
+      if (m1 != m2) {
+        // TODO: Added exeption
+      }
+
+      sparse_matrix tmp;
+      auto it_m1 = begin();
+      auto it_m2 = other.cbegin();
+      for (; it_m1 != end(); ++it_m1, ++it_m2) {
+        tmp[std::get<0>(*it_m1)][std::get<1>(*it_m1)] = std::get<2>(*it_m1) +
+                                                        std::get<2>(*it_m2);
+      }
+      return tmp;
+    }
+
+    /**
+     * @brief operator +=
+     * @param other
+     * @return
+     */
+    sparse_matrix operator+=(const sparse_matrix &other) {
+      const std::tuple<size_t, size_t> m1 = size();
+      const std::tuple<size_t, size_t> m2 = other.size();
+      if (m1 != m2) {
+        // TODO: Added exeption
+      }
+
+      auto it_m1 = begin();
+      auto it_m2 = other.cbegin();
+      for (; it_m1 != end(); ++it_m1, ++it_m2) {
+        std::get<2>(*it_m1) += std::get<2>(*it_m2);
+      }
+      return *this;
+    }
+
+    /**
+     * @brief Comparison operator.
+     * @param other [in] - the object to compare.
      */
     bool operator==(const sparse_matrix &other) const {
       return data_ == other.data_;
     }
 
     /**
-     *  @brief Access operator by index.
-     *  @param index [in] - matrix index.
+     * @brief Comparison operator.
+     * @param other [in] - the object to compare.
+     */
+    bool operator!=(const sparse_matrix &other) const {
+      return data_ != other.data_;
+    }
+
+    /**
+     * @brief Access operator by index.
+     * @param index [in] - matrix index.
      */
     next_index operator[](const size_t index) {
       return next_index(static_cast<std::tuple<size_t>>(index), data_);
     }
 
     /**
-     *  @brief Constant access operator by index.
-     *  @param index [in] - matrix index.
+     * @brief Constant access operator by index.
+     * @param index [in] - matrix index.
      */
     const next_index operator[](const size_t index) const {
       return next_index(static_cast<std::tuple<size_t>>(index), data_);
@@ -160,7 +262,7 @@ class sparse_matrix
      * @return Returns an const iterator to the beginning of the matrix.
      */
     const auto cbegin() const {
-      return iterator_matrix(data_.cbegin());
+      return const_iterator_matrix(data_.cbegin());
     }
 
     /**
@@ -176,14 +278,14 @@ class sparse_matrix
      * @return Returns an const iterator to the end of the matrix.
      */
     const auto cend() const {
-      return iterator_matrix(data_.cend());
+      return const_iterator_matrix(data_.cend());
     }
 
     /**
      * @brief The number of calls occupied by the data matrix.
      * @return The number of occupied calls.
      */
-    size_t size() const {
+    size_t data_count() const {
       return data_.size();
     }
 
@@ -194,33 +296,64 @@ class sparse_matrix
       data_.clear();
     }
 
+    /**
+     *
+     */
+    size_t m_size() {
+      return std::get<0>(size()) + 1;
+    }
+
+    /**
+     *
+     */
+    size_t n_size() {
+      return std::get<1>(size()) + 1;
+    }
+
 
   private:
-    std::map<index_t, value_type_t> data_;            /** - data container. */
-    const value_type_t default_value_{default_value}; /** - default value. */
+    std::map<index_t, T> data_;            /**< - data container. */
+    const T default_value_{DEFAULT_VALUE}; /**< - default value. */
+
+    /**
+     * @brief add
+     * @param idx_m
+     * @param idx_n
+     * @param value
+     */
+    void add(unsigned idx_m, unsigned idx_n, T value) {
+      data_[std::make_tuple(idx_m, idx_n)] = value;
+    }
+
+    /**
+     *
+     */
+    const std::tuple<size_t, size_t> & size() const {
+      return (--data_.end())->first;
+    }
 };
 
 
 /**
  * Class to work with matrix index.
  */
-template<typename value_type_t, value_type_t default_value, size_t matrix_size>
+template<typename T, T DEFAULT_VALUE, size_t MATRIX_SIZE>
 template<size_t num, typename... types_t>
-class sparse_matrix<value_type_t, default_value, matrix_size>::index_matrix
+class sparse_matrix<T, DEFAULT_VALUE, MATRIX_SIZE>::index_matrix
 {
   private:
     /* Aliases */
     using next_index = index_matrix<num - 1, types_t..., size_t>;
 
-    const sparse_matrix::contener_t &data_; /** - container with data. */
-    std::tuple<types_t...> index_view_;     /** - representation of the index
-                                             *    as a tuple. */
+    const sparse_matrix::contener_t &data_; /**< - container with data. */
+    std::tuple<types_t...> index_view_;     /**< - representation of the index
+                                             *     as a tuple. */
 
   public:
     /**
-     *  @brief The constructor.
-     *  @param index_view [in] - matrix index.
-     *  @param data [in] - contener.
+     * @brief The constructor.
+     * @param index_view [in] - matrix index.
+     * @param data [in] - contener.
      */
     index_matrix(std::tuple<types_t...> index_view,
                  const sparse_matrix::contener_t &data)
@@ -228,16 +361,16 @@ class sparse_matrix<value_type_t, default_value, matrix_size>::index_matrix
     {}
 
     /**
-     *  @brief Access operator by index.
-     *  @param index [in] - matrix index.
+     * @brief Access operator by index.
+     * @param index [in] - matrix index.
      */
     next_index operator[](size_t index) {
       return next_index(std::tuple_cat(index_view_, std::tie(index)), data_);
     }
 
     /**
-     *  @brief Constant access operator by index.
-     *  @param index [in] - matrix index.
+     * @brief Constant access operator by index.
+     * @param index [in] - matrix index.
      */
     const next_index operator[](size_t index) const {
       return next_index(std::tuple_cat(index_view_, std::tie(index)), data_);
@@ -248,22 +381,21 @@ class sparse_matrix<value_type_t, default_value, matrix_size>::index_matrix
 /**
  * Class to work with matrix index, if index = 0.
  */
-template<typename value_type_t, value_type_t default_value, size_t matrix_size>
+template<typename T, T DEFAULT_VALUE, size_t MATRIX_SIZE>
 template<typename... types_t>
-class sparse_matrix<value_type_t, default_value, matrix_size>::
-    index_matrix<0, types_t...>
+class sparse_matrix<T, DEFAULT_VALUE, MATRIX_SIZE>::index_matrix<0, types_t...>
 {
   private:
-    const sparse_matrix::contener_t &data_;   /** - container with data. */
-    std::tuple<types_t...> index_view_;       /** - representation of the index
-                                               *    as a tuple. */
-    const value_type_t default_value_{default_value};
+    const sparse_matrix::contener_t &data_;   /**< - container with data. */
+    std::tuple<types_t...> index_view_;       /**< - representation of the
+                                               *     index as a tuple. */
+    const T default_value_{DEFAULT_VALUE};
 
   public:
     /**
-     *  @brief The constructor.
-     *  @param index_view [in] - matrix index.
-     *  @param data [in] - contener.
+     * @brief The constructor.
+     * @param index_view [in] - matrix index.
+     * @param data [in] - contener.
      */
     index_matrix(std::tuple<types_t...> index_view,
                  const sparse_matrix::contener_t &data)
@@ -271,10 +403,10 @@ class sparse_matrix<value_type_t, default_value, matrix_size>::
     {}
 
     /**
-     *  @brief Copy operator.
-     *  @param value [in] - matrix value.
+     * @brief Copy operator.
+     * @param value [in] - matrix value.
      */
-    auto & operator=(const value_type_t &value) {
+    auto & operator=(const T &value) {
       if (value != default_value_)
         const_cast<sparse_matrix::contener_t &>(data_)[index_view_] = value;
       else {
@@ -288,7 +420,7 @@ class sparse_matrix<value_type_t, default_value, matrix_size>::
     /**
      * Conversion operator.
      */
-    operator const value_type_t &() const {
+    operator const T &() const {
       auto it = data_.find(index_view_);
       return (it != data_.cend()) ? it->second : default_value_;
     }
@@ -296,45 +428,146 @@ class sparse_matrix<value_type_t, default_value, matrix_size>::
 
 
 /**
- *  Class that implements an iterator for working with matrix data.
+ * Class that implements an iterator for working with matrix data.
  */
-template<typename value_type_t, value_type_t default_value, size_t matrix_size>
-class sparse_matrix<value_type_t, default_value, matrix_size>:: iterator_matrix
+template<typename T, T DEFAULT_VALUE, size_t MATRIX_SIZE>
+class sparse_matrix<T, DEFAULT_VALUE, MATRIX_SIZE>::iterator_matrix
 {
   private:
     /* Aliases */
-    using iterator_t = typename sparse_matrix::contener_t::const_iterator;
+    using iterator_t = typename sparse_matrix::contener_t::iterator;
     using iterator_category_t = std::input_iterator_tag;
 
-    iterator_t map_iterator_;
+    iterator_t map_iterator_;   /**< - iterator of the data. */
 
 
   public:
    using value_t = decltype(std::tuple_cat((*map_iterator_).first,
                                           std::tie((*map_iterator_).second)));
 
+    /**
+     * @brief The constructor.
+     * @param map_iterator [in] - iterator.
+     */
     explicit iterator_matrix(iterator_t map_iterator)
-      : map_iterator_(map_iterator) {}
+      : map_iterator_(map_iterator)
+    {}
 
+    /**
+     * @brief  Increment operator, prefix form.
+     * @return Increment data.
+     */
     iterator_matrix & operator++() {
-      map_iterator_++;
+      ++map_iterator_;
       return *this;
     }
 
+    /**
+     * @brief  Increment operator, postfix form.
+     * @return Increment data.
+     */
     iterator_matrix operator++(int) {
       iterator_matrix retval = *this;
       ++(*this);
       return retval;
     }
 
+    /**
+     * @brief  Comparison operator.
+     * @param  other [in] - iterator.
+     * @return The result of the comparison (false/true).
+     */
     bool operator==(iterator_matrix other) const {
       return map_iterator_ == other.map_iterator_;
     }
 
+    /**
+     * @brief  Inequality operator.
+     * @param  other [in] - iterator.
+     * @return The result of the comparison (false/true).
+     */
     bool operator!=(iterator_matrix other) const {
       return !(*this == other);
     }
 
+    /**
+     * @brief  Dereference operator.
+     * @return Result.
+     */
+    value_t operator*() {
+      return std::tuple_cat((*map_iterator_).first,
+                            std::tie((*map_iterator_).second));
+    }
+};
+
+
+/**
+ * Class that implements an const iterator for working with matrix data.
+ */
+template<typename T, T DEFAULT_VALUE, size_t MATRIX_SIZE>
+class sparse_matrix<T, DEFAULT_VALUE, MATRIX_SIZE>::const_iterator_matrix
+{
+  private:
+    /* Aliases */
+    using iterator_t = typename sparse_matrix::contener_t::const_iterator;
+    using iterator_category_t = std::input_iterator_tag;
+
+    iterator_t map_iterator_;   /**< - iterator of the data. */
+
+
+  public:
+   using value_t = decltype(std::tuple_cat((*map_iterator_).first,
+                                          std::tie((*map_iterator_).second)));
+
+    /**
+     * @brief The constructor.
+     * @param map_iterator [in] - iterator.
+     */
+    explicit const_iterator_matrix(iterator_t map_iterator)
+      : map_iterator_(map_iterator)
+    {}
+
+    /**
+     * @brief  Increment operator, prefix form.
+     * @return Increment data.
+     */
+    const_iterator_matrix & operator++() {
+      ++map_iterator_;
+      return *this;
+    }
+
+    /**
+     * @brief  Increment operator, postfix form.
+     * @return Increment data.
+     */
+    const_iterator_matrix operator++(int) {
+      iterator_matrix retval = *this;
+      ++(*this);
+      return retval;
+    }
+
+    /**
+     * @brief  Comparison operator.
+     * @param  other [in] - iterator.
+     * @return The result of the comparison (false/true).
+     */
+    bool operator==(const_iterator_matrix other) const {
+      return map_iterator_ == other.map_iterator_;
+    }
+
+    /**
+     * @brief  Inequality operator.
+     * @param  other [in] - iterator.
+     * @return The result of the comparison (false/true).
+     */
+    bool operator!=(const_iterator_matrix other) const {
+      return !(*this == other);
+    }
+
+    /**
+     * @brief  Dereference operator.
+     * @return Result.
+     */
     value_t operator*() const {
       return std::tuple_cat((*map_iterator_).first,
                             std::tie((*map_iterator_).second));
